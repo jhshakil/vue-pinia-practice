a3d343ead539d066cdde3a0b4c4a188a855ee66c
Shakil <shakil11271@gmail.com>
2023-01-23 14:01:34 +0600

    add pinia

---
+++ src/stores/TaskStore.js
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia'
+
+export const useTaskStore = defineStore('taskStore', {
+    state: () => ({
+        tasks: [
+            { id: 1, title: "first task", isFave: false },
+            { id: 2, title: "second task", isFave: true },
+        ],
+        name: "jahid",
+    })
+})
